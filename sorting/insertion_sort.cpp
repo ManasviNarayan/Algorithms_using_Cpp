e0fff59ce5260e8bb6b82b403a5e6eac04a87089
Manasvi Narayan <51746563+ManasviNarayan@users.noreply.github.com>
2021-09-15 10:32:48 +0530

    Update insertion_sort.cpp

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -14,13 +14,7 @@ void insertion_sort(int arr[], int n)
             j=j-1;
         }
         arr[j+1] = temp;
-        ////////////////////////////
-        for (int k = 0; k < n; k++)
-        {
-            cout << arr[k] << " ";
-        }
-        cout<<endl;
-        ////////////////////////////
+        
     }
     cout<<"Sorted array"<<endl;
     for(int i=0; i<n;i++)
